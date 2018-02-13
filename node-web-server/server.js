3d07ff913849883e28df0cd7c709d5c24d5a5152
uaday <uadayghosh@gmail.com>
2018-02-14 02:15:28 +0600

    update_14022018-02

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,14 @@ const express=require('express');
 var app=express();
 
 app.get('/',(request,response)=>{
-  response.send('Hello Express');
+  // response.send('<h1>Hello Express</h1>');
+  response.send({
+    name: 'Sudipta Ghosh',
+    Course:['c++','c','Java','PHP']
+  });
 });
 
+app.get('/about',(req,resp)=>{
+  resp.send('About Page');
+});
 app.listen(3000);
