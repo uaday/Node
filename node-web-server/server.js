3675d52e5d0d2a7045467fb25f9f6d8437de4a0f
uaday <uadayghosh@gmail.com>
2018-02-16 23:14:52 +0600

    update_16022018(express template engine handlebars)

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,20 +1,31 @@
 const express=require('express');
-
+const hbs=require('hbs');
 var app=express();
+
+app.use(express.static(__dirname+'/public'));
+
+app.set('view engine','hbs');
+
 
 app.get('/',(request,response)=>{
   // response.send('<h1>Hello Express</h1>');
-  response.send({
-    name: 'Sudipta Ghosh',
-    Course:['c++','c','Java','PHP']
-  });
+  response.render('home.hbs',{
+      welcomeMessage:'Welcome to you our site',
+      pageTitle: 'Home Page',
+      currentYear: new Date().getFullYear()
+  })
 });
 
 app.get('/about',(req,resp)=>{
-  resp.send('About Page');
+  resp.render('about.hbs',{
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  })
 });
 
 app.get('/contact',(req,resp)=>{
   resp.send('<h1>Contact Page</h1>')
 });
-app.listen(3000);
+app.listen(3000,()=>{
+  console.log('Server is up on port 3000');
+});
