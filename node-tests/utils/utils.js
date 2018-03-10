34d5b08b22c6a34771887148964a148c518c0aec
uaday <uadayghosh@gmail.com>
2018-03-11 02:19:43 +0600

    update_11032018

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -2,3 +2,10 @@ module.exports.add=(a,b)=>a+b;
 module.exports.min=(a,b)=>a-b;
 
 module.exports.square=(x)=>x*x;
+
+module.exports.setName=(user,fullName)=>{
+  var names=fullName.split(' ');
+  user.firstName=names[0];
+  user.lastName=names[1];
+  return user;
+};
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,20 +1,49 @@
+const expect =require('expect');
 const utils=require('./utils');
 it('should add two numbers',()=>{
   var res=utils.add(33,11);
-  if(res !== 44){
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+
+  expect(res).toBe(44).toBeA('number');
+
+  // if(res !== 44){
+  //   throw new Error(`Expected 44, but got ${res}`);
+  // }
 });
 it('should minus two numbers',()=>{
-  var res=utils.add(33,11);
-  if(res !== 22){
-    throw new Error(`Expected 22, but got ${res}`);
-  }
+  var res=utils.min(33,11);
+  expect(res).toExist().toBe(22).toBeA('number');
+  // if(res !== 22){
+  //   throw new Error(`Expected 22, but got ${res}`);
+  // }
 });
-
 it('should square a number',()=>{
   var res1=utils.square(3);
-  if(res1!==9){
-    throw new Error(`Expected 9, but got ${res1 }`);
-  }
+  expect(res1).toExist().toBe(9).toBeA('number');
+  // if(res1!==9){
+  //   throw new Error(`Expected 9, but got ${res1 }`);
+  // }
 });
+
+it('should verify first name and last name',()=>{
+  var user={
+    age:23,
+    location:'Dhanmondi'
+  };
+  var res=utils.setName(user,'Sudipta Ghosh');
+  // expect(user).toEqual(res);
+  expect(res).toInclude({firstName: 'Sudipta',lastName: 'Ghosh'});
+});
+
+// it('should expect some value',()=>{
+//     //expect(12).toNotBe(12);
+//     //expect({name: 'sudipta'}).toEqual({name:'Sudipta'});
+//     // expect([2,3,4]).toInclude(4);
+//     // expect([2,3,4]).toExclude(5);
+//     expect({
+//       name:'Sudipta',
+//       age: 23,
+//       location:'Bangladesh'
+//     }).toInclude({
+//       age:23
+//     });
+// });
