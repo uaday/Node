7f86979a17167392a7bf22ae04dd08514a59a8ee
uaday ghosh <uadayghosh@gmail.com>
2018-03-31 18:26:13 +0600

    update_31032018

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,20 @@
+const MongoClient=require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to the MongoDB server');
+
+  db.collection('Toods').insertOne({
+    text:'Something to do',
+    completed:false
+  },(err,result)=>{
+    if (err) {
+      return console.log('Unable to insert todo',err);
+    }
+    console.log(JSON.stringify(result.ops, undefined , 2));
+  });
+
+  db.close();
+});
