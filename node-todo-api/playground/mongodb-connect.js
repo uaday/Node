1cd4167dde4326eb0b22d10f1cf3a7bde3134734
uaday ghosh <uadayghosh@gmail.com>
2018-04-02 18:19:14 +0600

    update_02042018

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,20 +1,26 @@
-const MongoClient=require('mongodb').MongoClient;
+// const MongoClient=require('mongodb').MongoClient;
+const {MongoClient,ObjectId}=require('mongodb');
+const url=`mongodb://localhost:27017`;
+const dbName='TodoApp3';
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
-  if (err) {
+var user={name:'Sudipta',age:23};
+var {name}=user;
+console.log(name);
+MongoClient.connect(url,(err,client)=>{
+  if(err){
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to the MongoDB server');
-
-  db.collection('Toods').insertOne({
-    text:'Something to do',
-    completed:false
+  const db=client.db(dbName);
+  db.collection('Users').insertOne({
+    name:'Sudipta Ghosh',
+    age:23,
+    location:'Dhaka, Bangladesh'
   },(err,result)=>{
-    if (err) {
-      return console.log('Unable to insert todo',err);
+    if(err){
+      return console.log('Unable to insert Users',err);
     }
-    console.log(JSON.stringify(result.ops, undefined , 2));
+    console.log(JSON.stringify(result.ops[0]._id.getTimestamp(),undefined,2));
   });
-
-  db.close();
+  client.close();
 });
