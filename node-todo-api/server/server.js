5a2bec543ee1e1646b8c7e5e0eda690149cc8770
uaday ghosh <uadayghosh@gmail.com>
2018-05-08 03:17:53 +0600

    update_08052018

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,6 +5,10 @@ var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
 
+
+
+
+
 var app=express();
 
 app.use(bodyParser.json());
@@ -34,14 +38,19 @@ app.get('/todos',(req,res)=>{
     })
 },(e)=>{
     res.status(400).send(e);
+});
+
+app.get('/todos/:id',(req,res)=>{
+    res.send(req.params);
 })
+
 
 app.listen(3000,()=>{
     console.log('Started on port 3000');
 });
 
 
-module.exports={app};
+// module.exports={app};
 
 // const newTodo=new Todo({
 //     text:'Testing Text'
