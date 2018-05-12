3be3e74cffe36c7c8c957b5af912c300cab91a58
uaday ghosh <uadayghosh@gmail.com>
2018-05-13 01:07:36 +0600

    get request with id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 
+var {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
@@ -40,9 +41,30 @@ app.get('/todos',(req,res)=>{
     res.status(400).send(e);
 });
 
+
+
+
+//GET /todos/12345
+
 app.get('/todos/:id',(req,res)=>{
-    res.send(req.params);
-})
+    var id=req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send({});
+    }else{
+        Todo.findById(id).then((todo)=>{
+            if(!todo){
+                res.status(404).send({});
+            }else{
+                res.status(200).send({todo});
+            }
+        },(e)=>{
+            res.status(400).send(e);
+        })
+    }
+},(e)=>{
+    res.status(400).send(e);
+});
+
 
 
 app.listen(3000,()=>{
