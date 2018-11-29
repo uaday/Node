38c8f7f9fc3f1d04fcfb90bb626d43c1a61901fb
uaday ghosh <uadayghosh@gmail.com>
2018-11-30 03:12:01 +0600

    push new brach

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -6,11 +6,8 @@ var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
 
-
-
-
-
 var app=express();
+const port=process.env.PORT||3000;
 
 app.use(bodyParser.json());
 
@@ -67,8 +64,8 @@ app.get('/todos/:id',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-    console.log('Started on port 3000');
+app.listen(port,()=>{
+    console.log(`Started on port ${port}`);
 });
 
 
